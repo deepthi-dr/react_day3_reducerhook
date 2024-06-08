0471473c5834f749614a5341b79759b642a0ecca
deepthi-dr <167419200+deepthi-dr@users.noreply.github.com>
2024-06-08 19:16:29 +0530

    Add files via upload

---
+++ todoapp_2/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import ToDoList from './todolist';
+
+function App() {
+  return (
+   <div>
+    <ToDoList></ToDoList>
+   </div>
+  );
+}
+
+export default App;
